cedcd67be031ea71c3343e2cd08e5a96eafbdab8
Bros12Lek <adgamesbalhego@hotmail.com>
2023-09-02 22:07:31 -0300

    Melhorias no script feitas

--- js/navBar.cjs
+++ js/navBar.cjs
@@ -1,9 +1,27 @@
 const menuBar = document.querySelector('.menu-bar');
-
+const main = document.querySelector('main');
+const header = document.querySelector('header')
 const aside = document.querySelector('aside');
+const divLogo = document.querySelector('.divLogo')
 
 menuBar.addEventListener('click',(event) =>{
     menuBar.style.transition = 'all 2s'
     menuBar.classList.toggle('actived');
     aside.classList.toggle('showUp');
 });
+
+main.addEventListener('click',(event) =>{
+    if(menuBar.classList.contains('actived')){
+        menuBar.style.transition = 'all 2s';
+        menuBar.classList.remove('actived');
+        aside.classList.remove('showUp');
+    }
+})
+
+divLogo.addEventListener('click', (event) =>{
+    if(menuBar.classList.contains('actived')){
+        menuBar.style.transition = 'all 2s';
+        menuBar.classList.remove('actived');
+        aside.classList.remove('showUp');
+    }
+})
